3921c3014206b2f9a9ce56fb3acbb38d2f32cfc8
Taras <tarashuledza14@gmail.com>
2022-10-09 16:35:07 +0300

    right program

--- Lab_03.1/Lab_03.1.cpp
+++ Lab_03.1/Lab_03.1.cpp
@@ -13,7 +13,7 @@ int main()
     cout << "x= "; cin >> x;
 
     A = 5 * exp(3 * x);
-
+    // спосіб 1: розгалуження в скороченій формі
     if (x <= -1)
         B = 3 + sin(abs(x));
     if (x > -1 || x <= 3)
@@ -23,8 +23,19 @@ int main()
 
     y = A - B;
 
-    cout << "y = " << y << endl;
+    cout << "1) y = " << y << endl;
+    // спосіб 2: розгалуження в повній формі
 
+    if (x <= -1)
+        B = 3 + sin(abs(x));
+    else
+        if (x > -1 || x <= 3)
+            B = 2 * exp(x / 4 - 1);
+        else
+            B = 7 - sqrt(2) * pow(x, 3);
+    y = A - B;
+    cout << "2) y = " << y << endl;
+ 
     cin.get();
     return 0;
 }
