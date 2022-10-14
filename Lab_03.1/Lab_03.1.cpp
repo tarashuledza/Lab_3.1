ec3adb83284834edded021e3765602c76afa2440
Taras <tarashuledza14@gmail.com>
2022-10-14 13:04:36 +0300

    right version

--- Lab_03.1/Lab_03.1.cpp
+++ Lab_03.1/Lab_03.1.cpp
@@ -16,7 +16,7 @@ int main()
     // спосіб 1: розгалуження в скороченій формі
     if (x <= -1)
         B = 3 + sin(abs(x));
-    if (x > -1 || x <= 3)
+    if (x > -1  && x <= 3)
         B = 2 * exp(x / 4 - 1);
     if (x > 3)
         B = 7 - sqrt(2) * pow(x, 3);
@@ -29,7 +29,7 @@ int main()
     if (x <= -1)
         B = 3 + sin(abs(x));
     else
-        if (x > -1 || x <= 3)
+        if (x > -1 && x <= 3)
             B = 2 * exp(x / 4 - 1);
         else
             B = 7 - sqrt(2) * pow(x, 3);
